f8f83e979312823203bffaf58fe3a502d0e4995c
Shawn McClelland <7469168+shawnmcclelland@users.noreply.github.com>
2025-06-26 16:54:00 -0700

    Update App.tsx
    add design tokens to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import "@/builderTokens";
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
